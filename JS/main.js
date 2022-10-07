8cbc7c38afea6ded955629f0d77a86d3439dafb1
ChrissH84 <horvatkristian1@gmail.com>
2022-10-07 18:35:02 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,53 @@
+/* Header - Section / Hamburger Menu */
+const menuBtn = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-nav');
+
+menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+});
+
+
+
+/* Hero Section, Moving Background */ 
+let sun = document.getElementById('sun');
+let mountainsBack = document.getElementById('mountains-back');
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let mountainsFront = document.getElementById('mountains-front');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    sun.style.top = value * 1 + 'px';
+    mountainsBack.style.top = value * 0.50 + 'px';
+    mountainsFront.style.top = value * 0.25 + 'px';
+    text.style.marginTop = value * 3.5 + 'px';
+    btn.style.marginTop = value * 3.5 + 'px';
+})
+
+/* Abaut Section */
+window.addEventListener('scroll', reveal);
+
+function reveal(){ 
+    var reveals = document.querySelectorAll('.revealOne');
+    
+    for(var i = 0; i < reveals.length; i++){
+        
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+        var activePoint = windowHeight - revealPoint;
+        if(revealTop < activePoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+/* Explore Section */
+
+
+
+
+
